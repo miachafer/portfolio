d870f1e66a1dd35d15815936b151d2e2804eaf28
miachafer <miachafer@gmail.com>
2023-06-26 13:06:55 -0300

    Add intro card component to home page

---
+++ src/components/IntroCard.jsx
@@ -0,0 +1,12 @@
+import Card from "react-bootstrap/Card"
+
+export default function IntroCard() {
+  return (
+    <Card>
+      <Card.Body>
+        Hi, my name is Mia! :)
+        I am a software developer based in Belo Horizonte, Brazil, and currently working at <a href="https://www.t-systems.com/de/en" target="_blank">T-Systems</a>. In my free time, I enjoy reading, practicing origami, learning new languages, and studying computer science.
+      </Card.Body>
+    </Card>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import Header from "../components/Header";
+import IntroCard from "../components/IntroCard";
 
 export default function Home () {
   return (
-    <Header />
+    <>
+      <Header />
+      <IntroCard />
+    </>
   )
 }
