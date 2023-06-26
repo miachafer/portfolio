9a1017c438d5b92e5b112ec0e2b825751a997f11
miachafer <miachafer@gmail.com>
2023-06-26 12:36:49 -0300

    Implement routing to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
     <Nav className="justify-content-end">
       <Nav.Item>
-        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link><Link to="/">Home</Link></Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/projects">Projects</Nav.Link>
+        <Nav.Link><Link to="/projects">Projects</Link></Nav.Link>
       </Nav.Item>
     </Nav>
   )
