9aca43a6350c176924dccb613f7d63a99f2493cd
miachafer <miachafer@gmail.com>
2023-06-26 22:34:32 -0300

    Update projects page to display more details on each project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,34 @@
-export default function Projects () {
+import { Row, Col, Image } from "react-bootstrap"
+import projects from "../projects.json"
+
+export default function Projects() {
   return (
-    <h1>Projects</h1>
+    <>
+      <h1>Projects</h1>
+      {projects && projects.map(project => {
+        return (
+          <Row className="mb-3">
+            <Col>
+              <Image src={project.image} fluid></Image>
+            </Col>
+            <Col>
+              <h2>{project.title}</h2>
+              {project.description}
+              <ul className="list-unstyled">
+                {project.technologies && project.technologies.map(tech => {
+                  return (
+                    <li key={tech.id} >{tech.technology}</li>
+                  )
+                })}
+              </ul>
+              <ul className="list-unstyled">
+                <li><a href={project.github}>View on Github</a></li>
+                <li><a href={project.link}>View on the web</a></li>
+              </ul>
+            </Col>
+          </Row>
+        )
+      })}
+    </>
   )
 }
