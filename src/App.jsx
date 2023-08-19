ab3cfa284b9a9b41c42e13a55bc369eecb40c670
miachafer <miachafer@gmail.com>
2023-08-19 14:29:36 -0300

    Update routing

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <Container>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/portfolio" element={<Home />} />
+          <Route path="/portfolio/projects" element={<Projects />} />
         </Routes>
         <Footer />
       </Container>
