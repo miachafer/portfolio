fa3bf65f5f92c1c82d703ed689e82ed79edcc2c4
miachafer <miachafer@gmail.com>
2023-08-19 14:37:13 -0300

    Fix routing links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <Container className="mb-4">
         <Row className="d-flex align-items-center">
           <Col xs={4} md={2}>
-            <Image src="\MCF.jpg" roundedCircle fluid></Image>
+            <Image src="MCF.jpg" roundedCircle fluid></Image>
           </Col>
           <Col xs={8} md={10}>
             <h1 className="ml-3">Mia Ferreira</h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ export default function Navbar() {
   return (
     <Nav className="mt-2 mb-2 justify-content-end">
       <Nav.Item>
-        <Nav.Link><Link to="/">Home</Link></Nav.Link>
+        <Nav.Link><Link to="/portfolio">Home</Link></Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link><Link to="/projects">Projects</Link></Nav.Link>
+        <Nav.Link><Link to="/portfolio/projects">Projects</Link></Nav.Link>
       </Nav.Item>
     </Nav>
   )
